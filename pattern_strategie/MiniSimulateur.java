cd48416b13b5fb3e2badc69688d6702111563804
Mohamed El Béchir <mohamedelbechir1@gmail.com>
2020-03-29 08:22:19 +0200

    pattern commande

--- pattern_strategie/MiniSimulateur.java
+++ pattern_strategie/MiniSimulateur.java
@@ -6,9 +6,7 @@ import pattern_strategie.client.ProtoTypeCanard;
 import pattern_strategie.comportement_de_vol.PropulsionAReaction;
 
 /**
- * MiniSimulateur : ! Le pattern Stratégie définit une famille
- * d’algorithmes, encapsule chacun d’eux et les rend interchangeables. Stratégie
- * permet à l’algorithme de varier indépendamment des clients qui l’utilisent.
+ * MiniSimulateur : 
  */
 public class MiniSimulateur {
 
